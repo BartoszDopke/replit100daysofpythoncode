248a85e1216be02989b357fa76c30e093460565b
Bartosz Dopke <bartek.dopke@wp.pl>
2024-09-09 20:01:36 +0200

    solution for day2

---
+++ day2/main.py
@@ -0,0 +1,8 @@
+name = input("What's your name? ")
+food = input("What's your favourite food? ")
+music = input("What's your favourite music? ") 
+place_to_live = input("Where do you live? ")
+print(f"""So your name is {name}, 
+you like eating {food}, 
+you like listening to the {music} and you live in {place_to_live}.
+You're really cool guy!""")
