d56896697315760bc436bd0edc9141091bc85cb0
Bartosz Dopke <bartek.dopke@wp.pl>
2024-09-11 21:01:55 +0200

    solution for day3

---
+++ day3/main.py
@@ -0,0 +1,7 @@
+food = input("Provide type of food: ")
+plant = input("Provide type of plant: ")
+method_of_cooking = input("Provide method of cooking: ")
+describe_burned_word = input("Provide a word to describe burned food: ")
+household_item = input("Provide a household item: ")
+
+print("Cooking", food, "with", describe_burned_word, plant, "on a", household_item)
